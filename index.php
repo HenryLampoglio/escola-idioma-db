18693487e8e7143a0cd005c27e9d9c26a06abfec
Guilherme Henrique Machado RIbeiro <100424449+Sh1inx@users.noreply.github.com>
2023-11-02 19:39:52 -0300

    Adição das páginas index e dashboard

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rocket & Grow</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+    <body>
+        <form>
+          Insira seu nome<br>
+          <input type="text"><br>
+          Insira sua senha<br>
+          <input type="password"><br>
+          <input type="submit">
+    </body>
+</html>
