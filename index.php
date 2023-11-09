224d5dfb4ca014558b697d01fd14023c3c636f3e
raphael <rafavfx1@gmail.com>
2023-11-08 22:50:53 -0300

    remocao de uma duplicata

--- index.php
+++ index.php
@@ -85,12 +85,6 @@ body{
             <p class="card-text">Diversas turmas disponíveis</p>
         </div>
     </div>
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="https://horario.com.br/wp-content/uploads/2019/11/03_geha_blog_formacao_das_turmas.jpg">
-        <div class="card-body">
-            <p class="card-text">Diversas turmas disponíveis</p>
-        </div>
-    </div>
 </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
